c6154756206a0e4f6e9e83dcdade429cb6903355
bei.liu <bei_liu_go@163.com>
2024-10-29 13:48:38 +0800

    trained model

--- scripts/reproduce_result_analysis.sh
+++ scripts/reproduce_result_analysis.sh
@@ -1,12 +1,35 @@
 
-source activate torch_gpu_3
+source activate pytorch_v1
 echo "change to conda in gpu server"
 echo "----begin to analysis result------"
 
-exp_dir=exp_demo_1
+exp_dir=trained_model
 
-model_dir=./$exp_dir/model_dir
-logger_file=$model_dir/analysis.log
-result_file=$model_dir/pred_all
 
-python -u dg_rdg/pred_result_ana.py --method confusion_matrix_detail  --logger_file $logger_file --result_file $result_file 
+# prepare-data.pickle
+
+echo "----begin identify model pred------"
+mkdir trained_model_pred
+
+read -r -p "continue?" input
+
+vcf_file=demo_data/demo.vcf
+used_snp_file=trained_model/final_model_700snp.snp_tokens
+
+echo "----VCF file SNP Check------"
+python dg_rdg/rjf_sample_feat_parse.py --method stat_vcf  --vcf_file $vcf_file --snp_file $used_snp_file  
+
+read -r -p "continue?" input
+echo "---VCF to AI Dataset---"
+data_file=trained_model_pred/new_sample.pickle
+python dg_rdg/rjf_sample_feat_parse.py --method parse_vcf --vcf_file $vcf_file --data_file $data_file --mock_group 1
+
+read -r -p "continue?" input
+echo "---do pred---"
+
+checkpoint=trained_model/snp_choose_main.model_checkpoint
+pred_log=trained_model_pred/new_sample_pred.log
+pred_result=trained_model_pred/pred_result
+
+python dg_rdg/rdc_classify_test.py --checkpoint $checkpoint --logger_file $pred_log  --data $data_file --test_result_file $pred_result
+
